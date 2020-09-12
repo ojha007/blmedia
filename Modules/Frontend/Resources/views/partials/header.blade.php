b1d302c9c2cbf5cd81b579a3fe89ea3ef8d06e3d
prakash pokhrel <talktoprakashpokhrel@gmail.com>
2020-09-13 00:54:10 +0545

    'change'

--- Modules/Frontend/Resources/views/partials/header.blade.php
+++ Modules/Frontend/Resources/views/partials/header.blade.php
@@ -2,7 +2,9 @@
     <!--header top start-->
     <section id="language_section">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <button class="navbar-toggler cancle_btn_open" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler cancle_btn_open" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                 <div class="toggle_img">
                     <img src="https://www.breaknlinks.com/hindi/themes/assets/images/language_icon.png" alt="">
                     <span>और संस्करण</span>
@@ -12,12 +14,17 @@
             <div class="collapse navbar-collapse cancle_btn_navbar" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto cancle_btn_close">
                     <span class="cancle_btn"><i class="fas fa-times"></i></span>
-                    <li><a href="https://www.breaknlinks.com/hindi/../nepali" class="am-bl-topscroll" target="_blank">नेपाली</a> </li>
-                    <li><a href="https://www.breaknlinks.com/hindi/../en" class="am-bl-topscroll" target="_blank">English</a> </li>
+                    <li><a href="https://www.breaknlinks.com/hindi/../nepali" class="am-bl-topscroll" target="_blank">नेपाली</a>
+                    </li>
+                    <li><a href="https://www.breaknlinks.com/hindi/../en" class="am-bl-topscroll" target="_blank">English</a>
+                    </li>
 
-                    <li><a href="https://www.breaknlinks.com/hindi/../spanish" class="am-bl-topscroll" target="_blank">Español</a> </li>
-                    <li><a href="https://www.breaknlinks.com/hindi/../chinese" class="am-bl-topscroll" target="_blank">中文网</a> </li>
-                    <li><a href="https://www.breaknlinks.com/hindi/../arabic" class="am-bl-topscroll" target="_blank">عربي</a> </li>
+                    <li><a href="https://www.breaknlinks.com/hindi/../spanish" class="am-bl-topscroll" target="_blank">Español</a>
+                    </li>
+                    <li><a href="https://www.breaknlinks.com/hindi/../chinese" class="am-bl-topscroll" target="_blank">中文网</a>
+                    </li>
+                    <li><a href="https://www.breaknlinks.com/hindi/../arabic" class="am-bl-topscroll" target="_blank">عربي</a>
+                    </li>
                 </ul>
             </div>
         </nav>
@@ -29,28 +36,41 @@
                     <h4>और संस्करण</h4>
                 </div>
                 <div class="scroll-news-content">
-                    <div class="tickercontainer"><div class="mask"><ul id="scrollNewsList" class="newsTickerLisst newsticker" style="width: 3454.32px; left: -2434.21px;">
-                                <li><a href="https://www.breaknlinks.com/hindi/../nepali" class="am-bl-topscroll" target="_blank">नेपाली</a> </li>
-                                <li><a href="https://www.breaknlinks.com/hindi/../en" class="am-bl-topscroll" target="_blank">English</a> </li>
+                    <div class="tickercontainer">
+                        <div class="mask">
+                            <ul id="scrollNewsList" class="newsTickerLisst newsticker"
+                                style="width: 3454.32px; left: -2434.21px;">
+                                <li><a href="https://www.breaknlinks.com/hindi/../nepali" class="am-bl-topscroll"
+                                       target="_blank">नेपाली</a></li>
+                                <li><a href="https://www.breaknlinks.com/hindi/../en" class="am-bl-topscroll"
+                                       target="_blank">English</a></li>
 
-                                <li><a href="https://www.breaknlinks.com/hindi/../spanish" class="am-bl-topscroll" target="_blank">Español</a> </li>
-                                <li><a href="https://www.breaknlinks.com/hindi/../chinese" class="am-bl-topscroll" target="_blank">中文网</a> </li>
-                                <li><a href="https://www.breaknlinks.com/hindi/../arabic" class="am-bl-topscroll" target="_blank">عربي</a> </li>
+                                <li><a href="https://www.breaknlinks.com/hindi/../spanish" class="am-bl-topscroll"
+                                       target="_blank">Español</a></li>
+                                <li><a href="https://www.breaknlinks.com/hindi/../chinese" class="am-bl-topscroll"
+                                       target="_blank">中文网</a></li>
+                                <li><a href="https://www.breaknlinks.com/hindi/../arabic" class="am-bl-topscroll"
+                                       target="_blank">عربي</a></li>
 
-                            </ul></div></div>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4 float-right">
                 <ul class="nav nav-pills">
 
                     <li class="nav-item">
-                        <form class="form-inline base-form" action="https://www.breaknlinks.com/hindi/search/execute_search" method="post">
+                        <form class="form-inline base-form"
+                              action="https://www.breaknlinks.com/hindi/search/execute_search" method="post">
                             <div class="form-group">
                                 <div class="input-group">
-                                    <input type="search" name="newsSearch" class="form-control" placeholder="Search..." required="">
+                                    <input type="search" name="newsSearch" class="form-control" placeholder="Search..."
+                                           required="">
 
                                     <div class="input-group-append">
-                                        <button type="submit" name="search" class="btn btn-trans"><i class="fa fa-search"></i></button>
+                                        <button type="submit" name="search" class="btn btn-trans"><i
+                                                    class="fa fa-search"></i></button>
                                     </div>
                                 </div>
                             </div>
@@ -59,7 +79,8 @@
                 </ul>
             </div>
             <div class="clearfix"></div>
-        </div></section>
+        </div>
+    </section>
     <!--ended header top-->
 
     <!--header mid start-->
@@ -80,7 +101,8 @@
                         <!--- Google Ads Start--->
                         <script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                         <!-- Aside Logo -->
-                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6371925597086820" data-ad-slot="3110130257" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6371925597086820"
+                             data-ad-slot="3110130257" data-ad-format="auto" data-full-width-responsive="true"></ins>
                         <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
@@ -109,10 +131,12 @@
                             <form class="form-inline base-form">
                                 <div class="form-group">
                                     <div class="input-group">
-                                        <input type="search" name="newsSearch" class="form-control" placeholder="Search..." required="">
+                                        <input type="search" name="newsSearch" class="form-control"
+                                               placeholder="Search..." required="">
 
                                         <div class="input-group-append">
-                                            <button type="submit" name="search" class="btn btn-trans"><i class="fa fa-search"></i></button>
+                                            <button type="submit" name="search" class="btn btn-trans"><i
+                                                        class="fa fa-search"></i></button>
                                         </div>
                                     </div>
                                 </div>
@@ -123,13 +147,15 @@
                 <div class="col-md-12 col-lg-10 p-0">
                     <nav class="navbar navbar-expand-lg navbar-light primary-nav navbar_fixed" id="stickyAM">
 
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#primaryNav" aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#primaryNav"
+                                aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="primaryNav">
                             <ul class="navbar-nav">
                                 <li class="nav-item active home-site-logo-home">
-                                    <a href="https://www.breaknlinks.com/hindi/" class="nav-link"><i class="fa fa-home"></i> <span class="sr-only">Home</span>
+                                    <a href="https://www.breaknlinks.com/hindi/" class="nav-link"><i
+                                                class="fa fa-home"></i> <span class="sr-only">Home</span>
                                     </a>
                                 </li>
                                 <li class="nav-item active home-site-logo">
@@ -139,7 +165,8 @@
 
                                 <li cclass="nav-item">
 
-                                    <a href="https://www.breaknlinks.com/hindi/category/blspecial-23" class="nav-link">बिएल स्पेशल</a>
+                                    <a href="https://www.breaknlinks.com/hindi/category/blspecial-23" class="nav-link">बिएल
+                                        स्पेशल</a>
 
 
                                 </li>
@@ -155,7 +182,8 @@
 
                                 <li cclass="nav-item">
 
-                                    <a href="https://www.breaknlinks.com/hindi/category/country-38-27-36" class="nav-link">देश</a>
+                                    <a href="https://www.breaknlinks.com/hindi/category/country-38-27-36"
+                                       class="nav-link">देश</a>
 
 
                                 </li>
@@ -163,7 +191,8 @@
 
                                 <li cclass="nav-item">
 
-                                    <a href="https://www.breaknlinks.com/hindi/category/break" class="nav-link">ब्रेक</a>
+                                    <a href="https://www.breaknlinks.com/hindi/category/break"
+                                       class="nav-link">ब्रेक</a>
 
 
                                 </li>
@@ -211,7 +240,8 @@
 
                                 <li cclass="nav-item">
 
-                                    <a href="https://www.breaknlinks.com/hindi/category/video" class="nav-link">वीडियो</a>
+                                    <a href="https://www.breaknlinks.com/hindi/category/video"
+                                       class="nav-link">वीडियो</a>
 
 
                                 </li>
@@ -219,12 +249,10 @@
 
                                 <li cclass="nav-item">
 
-                                    <a href="https://www.breaknlinks.com/hindi/category/literature-18" class="nav-link">साहित्य     </a>
+                                    <a href="https://www.breaknlinks.com/hindi/category/literature-18" class="nav-link">साहित्य </a>
 
 
                                 </li>
-
-
 
 
                             </ul>
@@ -236,12 +264,16 @@
                 <div class="col-md-12 col-lg-2 pl-0">
 
                     <ul class="follow-social-media">
-                        <li> <button type="button" class="btn btn-link">फलाे करें</button></li>
                         <li>
-                            <a href="https://www.facebook.com/breaknlinkshd/" target="_blank"><i class="fa fa-facebook-square"></i></a>
+                            <button type="button" class="btn btn-link">फलाे करें</button>
                         </li>
                         <li>
-                            <a href="https://twitter.com/breaknlinksin" target="_blank"><i class="fa fa-twitter-square"></i></a>
+                            <a href="https://www.facebook.com/breaknlinkshd/" target="_blank"><i
+                                        class="fa fa-facebook-square"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/breaknlinksin" target="_blank"><i
+                                        class="fa fa-twitter-square"></i></a>
                         </li>
                     </ul>
 
@@ -251,3 +283,16 @@
     </section>
     <!--ended main navigation bar-->
 </header>
+@push('scripts')
+    <script>
+        $(document).ready(function () {
+            $(window).scroll(function () {
+                var sticky = $('.amnavnd'),
+                    scroll = $(window).scrollTop();
+
+                if (scroll >= 150) sticky.addClass('headerfixed');
+                else sticky.removeClass('headerfixed');
+            });
+        });
+    </script>
+@endpush
