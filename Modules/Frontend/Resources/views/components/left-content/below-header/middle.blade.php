b25251ca3acfc71c53966229bdd1b26fb8283778
ojha007 <ojhaamir007@gmail.com>
2020-09-16 18:31:22 +0545

    fixes merge

--- Modules/Frontend/Resources/views/components/left-content/below-header/middle.blade.php
+++ Modules/Frontend/Resources/views/components/left-content/below-header/middle.blade.php
@@ -1,20 +1,6 @@
 <div class="newsBlock type-2 " id="BL_link">
-
-    <div class="block-header gn-heading">
-
-        <h2>
-            @if(count($secondPositionNews))
-                <a href="{{route($routePrefix.'news-category.show',$secondPositionNews->first()->category_slug)}}">
-                    {{$secondPositionNews->first()->categories}}
-                </a>
-            @endif
-        </h2>
-
-        @if(count($secondPositionNews))
-
     @if(count($secondPositionNews))
         <div class="block-header gn-heading">
-
 
             <h2>
                 <a href="{{route($routePrefix.'news-category.show',$secondPositionNews->first()->category_slug)}}">
@@ -32,37 +18,8 @@
                             {{--                        <a href="{{route($routePrefix.'news.show',$news->slug)}}">--}}
                             {{--                            <img src="{{$news->image}}" alt="{{$news->image_description}}"--}}
                             {{--                                 title="{{$news->image_title}}" class="img responsive-img">--}}
-                            {{--                        </a>--}}
+{{--                </a>--}}
                         </figure>
-
-
-                    <div class="news-content">
-                        <h4 class="news-title"><a
-                                    href="{{route($routePrefix.'news.show',$news->slug)}}">
-                                {{$news->title}}
-                            </a></h4>
-                        <ul class="hr-list wide stamp">
-                            <li><i class="fa fa-circle blus"></i>{{$news->guest_name}}</li>
-                        </ul>
-                        <p> {{$news->sub_title}}</p>
-                    </div>
-                </div>
-            @else
-                <div class="news-item">
-                    <figure class="news-image">
-                        <a href="{{route($routePrefix.'news.show',$news->slug)}}">
-                            <img src="{{$news->image}}" alt="{{$news->image_description}}"
-                                 title="{{$news->image_title}}">
-                        </a>
-                        <ul class="hr-list wide stamp">
-                            <li><i class="fa fa-circle blus"></i>{{$news->guest_name}}</li>
-                        </ul>
-                    </figure>
-                    <div class="news-content">
-                        <h4 class="news-title"><a
-                                    href="{{route($routePrefix.'news.show',$news->slug)}}">
-                                {{$news->title}}
-                            </a></h4>
 
                         <div class="news-content">
                             <h4 class="news-title"><a
@@ -92,25 +49,14 @@
                                     {{$news->title}}
                                 </a></h4>
 
-
                             <p>{{$news->sub_title}}</p>
 
                         </div>
                     </div>
-
-                </div>
-            @endif
-        @endforeach
-        @if(count($secondPositionNews))
-            <a href="{{route($routePrefix.'news-category.show',$secondPositionNews->first()->category_slug)}}"
-               class=" btn btn-viewAll float-right" role="button">सब</a>
-        @endif
-    </div>
                 @endif
             @endforeach
             <a href="{{route($routePrefix.'news-category.show',$secondPositionNews->first()->category_slug)}}"
                class=" btn btn-viewAll float-right" role="button">सब</a>
         </div>
     @endif
-
 </div>
