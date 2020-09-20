be689f978a627ae81211e514d52397a322212dd0
nutan.dahal+2-at-643827904245 </j7RX3op5yrCfZss4yoNpbQGCG3HNeiu64fQzW4uTDs=>
2020-09-20 21:01:47 +0545

    changes on sidebar

--- Modules/Backend/Resources/views/partials/sidebar.blade.php
+++ Modules/Backend/Resources/views/partials/sidebar.blade.php
@@ -1,27 +1,27 @@
 <!-- sidebar: style can be found in sidebar.less -->
 <section class="sidebar">
     <!-- Sidebar user panel -->
-    <div class="user-panel">
-        <div class="pull-left image">
-            <img src="#" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-            <p>Alexander Pierce</p>
-            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-    </div>
+{{--    <div class="user-panel">--}}
+{{--        <div class="pull-left image">--}}
+{{--            <img src="#" class="img-circle" alt="User Image">--}}
+{{--        </div>--}}
+{{--        <div class="pull-left info">--}}
+{{--            <p>Alexander Pierce</p>--}}
+{{--            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
     <!-- search form -->
-    <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-            <input type="text" name="q" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-    </form>
+{{--    <form action="#" method="get" class="sidebar-form">--}}
+{{--        <div class="input-group">--}}
+{{--            <input type="text" name="q" class="form-control" placeholder="Search...">--}}
+{{--            <span class="input-group-btn">--}}
+{{--                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>--}}
+{{--                </button>--}}
+{{--              </span>--}}
+{{--        </div>--}}
+{{--    </form>--}}
     <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">MAIN NAVIGATION</li>
+{{--        <li class="header">MAIN NAVIGATION</li>--}}
         <li class="{{request()->is($urlPrefix) ? 'active' :' '}}">
             <a href="{{route($routePrefix.'.')}}">
                 <i class="fa fa-dashboard"></i>
