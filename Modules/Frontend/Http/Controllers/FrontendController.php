d1e86360b21bd6c3cc0fa2cf6cb89142b86a4c20
ojha007 <ojhaamir007@gmail.com>
2020-09-22 00:55:15 +0545

    video section

--- Modules/Frontend/Http/Controllers/FrontendController.php
+++ Modules/Frontend/Http/Controllers/FrontendController.php
@@ -53,6 +53,7 @@ class FrontendController extends Controller
         $firstPositionNews = $this->newsRepository->getNewsByPosition(1, 9);
         $secondPositionNews = $this->newsRepository->getNewsByPosition(2, 5);
         $thirdPositionNews = $this->newsRepository->getNewsByPosition(3, 8);
+        $fourthPositionNews = $this->newsRepository->getNewsByPosition(4, 4);
         $fifthPositionNews = $this->newsRepository->getNewsByPosition(5, 5);
         $sixthPositionNews = $this->newsRepository->getNewsByPosition(6, 10);
         $seventhPositionNews = $this->newsRepository->getNewsByPosition(7, 6);
@@ -71,7 +72,8 @@ class FrontendController extends Controller
             'eleventhPositionNews', 'twelvePositionNews',
             'ads_above_top_menu', 'ads_below_top_menu',
             'ads_aside_logo',
-            'thirteenPositionNews','fourteenPositionNews'));
+            'fourthPositionNews',
+            'thirteenPositionNews', 'fourteenPositionNews'));
 
     }
 
@@ -102,31 +104,31 @@ class FrontendController extends Controller
 
     }
 
-   /* public function singlePage()
-    {
-        $ads_above_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'above', 2);
-        $ads_below_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'below', 2);
-        $ads_aside_logo = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'aside', 1);
-        $firstPositionNews = $this->newsRepository->getNewsByPosition(1, 9);
-        $secondPositionNews = $this->newsRepository->getNewsByPosition(2, 5);
-        $thirdPositionNews = $this->newsRepository->getNewsByPosition(3, 8);
-        return view('frontend::components.single-page', compact(
-            'firstPositionNews', 'secondPositionNews', 'thirdPositionNews',
-            'ads_above_top_menu', 'ads_below_top_menu',
-            'ads_aside_logo'));
-    }
-    public function detailPage()
-    {
-        $firstPositionNews = $this->newsRepository->getNewsByPosition(1, 9);
-        $ads_above_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'above', 2);
-        $ads_below_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'below', 2);
-        $ads_aside_logo = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'aside', 1);
-        $secondPositionNews = $this->newsRepository->getNewsByPosition(2, 5);
-        $thirdPositionNews = $this->newsRepository->getNewsByPosition(3, 8);
-        return view('frontend::components.detail-page', compact(
-            'firstPositionNews', 'secondPositionNews', 'thirdPositionNews',
-            'ads_above_top_menu', 'ads_below_top_menu',
-            'ads_aside_logo'));
-    }*/
+    /* public function singlePage()
+     {
+         $ads_above_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'above', 2);
+         $ads_below_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'below', 2);
+         $ads_aside_logo = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'aside', 1);
+         $firstPositionNews = $this->newsRepository->getNewsByPosition(1, 9);
+         $secondPositionNews = $this->newsRepository->getNewsByPosition(2, 5);
+         $thirdPositionNews = $this->newsRepository->getNewsByPosition(3, 8);
+         return view('frontend::components.single-page', compact(
+             'firstPositionNews', 'secondPositionNews', 'thirdPositionNews',
+             'ads_above_top_menu', 'ads_below_top_menu',
+             'ads_aside_logo'));
+     }
+     public function detailPage()
+     {
+         $firstPositionNews = $this->newsRepository->getNewsByPosition(1, 9);
+         $ads_above_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'above', 2);
+         $ads_below_top_menu = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'below', 2);
+         $ads_aside_logo = $this->adsRepository->getAdsByForAndSubForAndPlacement('main_page', 'top_menu', 'aside', 1);
+         $secondPositionNews = $this->newsRepository->getNewsByPosition(2, 5);
+         $thirdPositionNews = $this->newsRepository->getNewsByPosition(3, 8);
+         return view('frontend::components.detail-page', compact(
+             'firstPositionNews', 'secondPositionNews', 'thirdPositionNews',
+             'ads_above_top_menu', 'ads_below_top_menu',
+             'ads_aside_logo'));
+     }*/
 
 }
--- Modules/Frontend/Resources/views/components/left-content/left-middle-1/left-middle-1.blade.php
+++ Modules/Frontend/Resources/views/components/left-content/left-middle-1/left-middle-1.blade.php
@@ -1,63 +1,85 @@
-<div class="section-row pt-0">
-    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 float-left">
-        <div class="BL-videogallery front_body_position_4" >
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="block-header gn-heading">
-                        <h2><a href="https://www.breaknlinks.com/hindi/category/video">वीडियो</a>
-                        </h2>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="videogallery-carousel owl-carousel owl-loaded owl-drag">
-                        <div class="owl-stage-outer">
-                            <div class="owl-stage" style="transform: translate3d(-1227px, 0px, 0px); transition: all 0.25s ease 0s; width: 3192px;">
-                                <div class="owl-item cloned"
-                                     style="width: 235.498px; margin-right: 10px;">
-                                    <div class="item">
-                                        <div class="video-gallery">
-                                            <div class="image-gallery">
-                                                <a href="https://www.breaknlinks.com/hindi/news/1840">
-                                                    <img src="https://www.breaknlinks.com/hindi/uploads/Indian%20leader/modi%20pm.jpg"
-                                                         title="PM मोदी ने स्वतंत्रता दिवस पर देशवासियों को दीं शुभकामनाएं">
-                                                </a>
-                                                <div class="overlay">
+@if(count($fourthPositionNews))
+    <div class="section-row pt-0">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 float-left">
+            <div class="BL-videogallery front_body_position_4">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="block-header gn-heading">
+                            <h2>
+                                <a href="{{route($routePrefix.'news-category.show',$fourthPositionNews->first()->category_slug)}}">
+                                    {{$fourthPositionNews->first()->categories}}
+                                </a>
+                            </h2>
 
-                                                    <a href="">
-                                                        <div class="overlay-image">
-                                                            <img src="https://www.breaknlinks.com/hindi/themes/images/play-icon.png"
-                                                                 alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="videogallery-carousel owl-carousel owl-loaded owl-drag">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage"
+                                     style="transform: translate3d(-1227px, 0px, 0px); transition: all 0.25s ease 0s; width: 3192px;">
+                                    <div class="owl-item cloned"
+                                         style="width: 235.498px; margin-right: 10px;">
+
+                                        <div class="item">
+
+                                            <div class="video-gallery">
+                                                @foreach($fourthPositionNews as $news)
+                                                    <div class="image-gallery">
+                                                        <a href="https://www.breaknlinks.com/hindi/news/1840">
+                                                            <img src="{{asset('frontend/img/orange.jpg')}}" alt=""
+                                                                 class="responsive-img">
+
+                                                            {{--                                                            <img--}}
+                                                            {{--                                                                src="https://www.breaknlinks.com/hindi/uploads/Indian%20leader/modi%20pm.jpg"--}}
+                                                            {{--                                                                title="PM मोदी ने स्वतंत्रता दिवस पर देशवासियों को दीं शुभकामनाएं">--}}
+                                                        </a>
+                                                        <div class="overlay">
+
+                                                            <a href="">
+                                                                <div class="overlay-image">
+                                                                    <img
+                                                                        src="https://www.breaknlinks.com/hindi/themes/images/play-icon.png"
+                                                                        alt="">
+                                                                </div>
+                                                            </a>
                                                         </div>
-                                                    </a>
-                                                </div>
+                                                    </div>
+                                                    <div class="text">
+                                                        <ul class="hr-list wide stamp">
+                                                            <li>
+                                                                <i class="fa fa-circle blus">
+
+                                                                </i>
+                                                                बिएल
+                                                                संवाददाता
+                                                            </li>
+                                                        </ul>
+                                                        <h5>
+                                                            <a href="{{route($routePrefix.'news.show',$news->slug)}}">
+                                                                {{$news->title}}
+                                                            </a></h5>
+                                                    </div>
+                                                @endforeach
                                             </div>
-                                            <div class="text">
-                                                <ul class="hr-list wide stamp">
-                                                    <li><i class="fa fa-circle blus"></i> बिएल
-                                                        संवाददाता
-                                                    </li>
-                                                </ul>
-                                                <h5>
-                                                    <a href="https://www.breaknlinks.com/hindi/news/1840">PM
-                                                        मोदी ने स्वतंत्रता दिवस पर देशवासियों को दीं
-                                                        शुभकामनाएं</a></h5>
-                                            </div>
+
+
                                         </div>
+
                                     </div>
                                 </div>
                             </div>
+                            {{--                            <div class="owl-nav disabled">--}}
+                            {{--                                <button type="button" role="presentation" class="owl-prev"><span--}}
+                            {{--                                        aria-label="Previous">‹</span></button>--}}
+                            {{--                                <button type="button" role="presentation" class="owl-next"><span--}}
+                            {{--                                        aria-label="Next">›</span></button>--}}
+                            {{--                            </div>--}}
                         </div>
-                        <div class="owl-nav disabled">
-                            <button type="button" role="presentation" class="owl-prev"><span
-                                        aria-label="Previous">‹</span></button>
-                            <button type="button" role="presentation" class="owl-next"><span
-                                        aria-label="Next">›</span></button>
-                        </div>
+                        {{--                    @include('frontend::components.buttons.view-all-category-button', ['position' => $tenthPositionNews])--}}
                     </div>
-{{--                    @include('frontend::components.buttons.view-all-category-button', ['position' => $tenthPositionNews])--}}
                 </div>
             </div>
         </div>
     </div>
-</div>
-
+@endif
